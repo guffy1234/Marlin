23abea8322fc28494283be717bb61575d0bfcc40
Guffy <guffy.forums@gmail.com>
2020-03-21 21:03:11 +0200

    changed config

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1609,7 +1609,7 @@
 //
 // G60/G61 Position Save and Return
 //
-//#define SAVED_POSITIONS 1         // Each saved position slot costs 12 bytes
+#define SAVED_POSITIONS 4         // Each saved position slot costs 12 bytes
 
 //
 // G2/G3 Arc Support
